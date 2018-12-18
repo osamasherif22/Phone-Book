8c900d81d9631a87712714718caf9f078ab1cddd
MohammedAimanHESSin <45923127+MohammedAimanHESSin@users.noreply.github.com>
2018-12-19 00:43:36 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-//#include <conio.h>
+#include <stdio.h> #include <stdlib.h>  #include <string.h> #include <conio.h>
 
 /** Declarations of Structures **/
 
@@ -11,101 +8,26 @@ typedef struct{
     int year;
 }birth;
 typedef struct {
-    char first_name[256];
-    char last_name[256];
+    char first_name[100];
+    char last_name[100];
     birth BD;
-    char adress[128];
-    char city[128];
+    char adress[100];
+    char email[100];
     int mob_num;
 }cont;
 
+
 /**Global Variable**/
-
-cont arr[256];
+cont arr[100];
 int counter=0;
+                                        /***** Menu Functions ****/
 
 
-/******************************************* SORT FUNCTION *******************************************/
-
-void bubble_sort()
-{
-for (int i=0 ; i<counter-1 ; i++)
-{
-    for (int j=0 ; j<counter-i-1;j++)
-    {
-        if((strcmp(arr[j].last_name,arr[j+1].last_name)>0))
-        {
-            cont temp ;
-            temp = arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-        }
-    }
-}
-}
-
-/******************************************** LOAD FUNCTION *******************************************/
-void LOAD ()
-{
-char filename[256] ;
-FILE *fRptr ;
-puts("enter your text name :");
-scanf("%s",filename);
-fRptr = fopen(filename,"r");
-if (fRptr == NULL)
-    {
-        printf("Cannot open file \n");
-        exit(0);
-    }
-int count=counter ;
-while(!feof(fRptr))
-{
-fscanf(fRptr,"%[^,],%s,%[^-]-%d-%d,%s,%s,%d\n",arr[count].first_name,arr[count].last_name,&arr[count].BD.day,&arr[count].BD.month,&arr[count].BD.year,arr[count].adress,arr[count].city,&arr[count].mob_num);
-/*fscanf(fRptr,"%s,",arr[count].last_name);
-fscanf(fRptr,"%[^-]-",&arr[count].BD.day);
-fscanf(fRptr,"%d-",&arr[count].BD.month);
-fscanf(fRptr,"%d,",&arr[count].BD.year);
-fscanf(fRptr,"%s,",arr[count].adress);
-fscanf(fRptr,"%s,",arr[count].city);
-fscanf(fRptr,"%d\n",&arr[count].mob_num);*/
-
-count++;
-}
-fclose(fRptr);
-printf("Uploaded successfully");
-char ch ;
-while ((ch = getchar()) != '\n' && ch != EOF) ;
-printf("\n\nPress ENTER to continue.");
-while ((ch = getchar()) != '\n' && ch != EOF) ;
-}
-
-/******************************************* SAVE FUNCTION *******************************************/
-
-void SAVE()
-{
-FILE*fptr;
-fptr=fopen("our_directory.txt","w");
-printf("%d",counter);
-for(int i=0;i<counter;i++)
-    {
-fprintf(fptr,"%s,%s,%d-%d-%d,%s,%s,%d\n",arr[i].first_name,arr[i].last_name,arr[i].BD.day,arr[i].BD.month,arr[i].BD.year,
-        arr[i].adress,arr[i].city,arr[i].mob_num);
-//fprintf(fptr,"\n");
-    }
-    fclose(fptr);
-    printf("Saved successfully");
-char ch ;
-while ((ch = getchar()) != '\n' && ch != EOF) ;
-printf("\n\nPress ENTER to continue.");
-while ((ch = getchar()) != '\n' && ch != EOF) ;
-}
-
-
-/******************************************* ADD FUNCTION *******************************************/
+/******************************************* (1) ADD FUNCTION *******************************************/
 
 void ADD(){
 
-   // void clrscr();
+    system("cls");
     int n ;
     printf("\n\nHow many contacts do you want to add : ");
     scanf("%d",&n);
@@ -124,8 +46,8 @@ void ADD(){
      scanf("%d",&arr[counter].BD.year);
         printf("Enter the address of the contact No.%d:",(i+1));
       scanf(" %[^\t\n]s",arr[counter].adress);
-        printf("Enter the city of the %d th contact:",(i+1));
-    scanf(" %[^\t\n]s",arr[counter].city);
+        printf("Enter the email of the %d th contact:",(i+1));
+    scanf(" %[^\t\n]s",arr[counter].email);
         printf("Enter the mobile number of the contact No.",(i+1));
   scanf("%d",&arr[counter].mob_num);
           printf("\n\n\n______________________________________________________\n");
@@ -138,24 +60,164 @@ printf("\n\nPress ENTER to continue.");
 while ((ch = getchar()) != '\n' && ch != EOF) ;
 }
 
+/******************************************** (2)LOAD FUNCTION *******************************************/
+void LOAD ()
+{
+system("cls");
+FILE *fRptr ;
+fRptr = fopen("tt.txt","r");
+if (fRptr == NULL)
+    {
+        printf("\nCannot open file ! \n");
+    }
+else {
+int count=counter ;
+while(!feof(fRptr))
+{
+fscanf(fRptr,"%[^,],%s,-%d-%d,%s,%s,%d\n",arr[count].first_name,arr[count].last_name,&arr[count].BD.day,&arr[count].BD.month,&arr[count].BD.year,arr[count].adress,arr[count].email,&arr[count].mob_num);
+/*fscanf(fRptr,"%[^,],",arr[count].last_name);
+fscanf(fRptr,"%[^,],",arr[count].last_name);
+fscanf(fRptr,"%d-",&arr[count].BD.day);
+fscanf(fRptr,"%d-",&arr[count].BD.month);
+fscanf(fRptr,"%d,",&arr[count].BD.year);
+fscanf(fRptr,"%[^\n,],",arr[count].adress);
+fscanf(fRptr,"%[^,],",arr[count].email);
+fscanf(fRptr,"%d\n",&arr[count].mob_num);*/
+
+count++;
+}
+fclose(fRptr);
+printf("\nUploaded successfully");
+}
+char ch ;
+while ((ch = getchar()) != '\n' && ch != EOF) ;
+printf("\n\nPress ENTER to continue.");
+while ((ch = getchar()) != '\n' && ch != EOF) ;
+}
+
+/******************************************* (3)MODIFY FUNCTION *******************************************/
 
 
+void MODIFY ()
+{
+system("cls");
+ bubble_sort();
+    char target[128] ;
+    puts("\nEnter last name of the contact you want to Modify in : ");
+   scanf("%s",target);
+   int j=1;
+    for ( int i =0; i < counter; i++)
+    {
+        if ((strcmp(target,arr[i].last_name)==0))
+        {
+            printf("\nContacts with this last name [%s] : \n ",target);
+  printf("contact number.%d\n",i);
+  printf("first name is : %s \n",arr[i].first_name);
+  printf("last name is :%s \n",arr[i].last_name);
+  printf("Date of birth is %d-%d-%d \n",arr[i].BD.day,arr[i].BD.month,arr[i].BD.year);
+  printf("address is : %s\n",arr[i].adress);
+  printf("email is :%s \n",arr[i].email);
+  printf("mobile number is : %d\n",arr[i].mob_num);
+  printf("\n"); j++;
+        }
+    }
+    int k;
+    printf("what is contact number does you want to Modify in :");
+    scanf("%d",&k);
+cont* p=&arr[k];
+int choice1;
+do
+{	printf("\n*** Enter what to modify ***\n\n");
+    printf( "1:Last name\n2:First name\n3:Address\n4:email\n5:Mobile number\n6:Nothing to modify\n" );
+    scanf("%d",&choice1);
 
-/***************************************** PRINT FUNCTION *******************************************/
+		switch(choice1)
+		{
+			case 1 : printf("enter new last name :");
+                     gets(p->last_name);
+		    case 2 : printf("enter new first name :");
+                     gets(p->first_name); ;
+            case 3 : printf("enter new address :");
+                     gets(p->adress);
+            case 4 : printf("enter new email :");
+                     gets(p->email);
+            case 5 : printf("enter new mobile number :");
+                     scanf("%d",&p->mob_num);
+            case 6 :
+
+                printf("\nyou are quiting !\n");
+				return(0);
+			default: printf("\nInvalid choice!\n");
+				break;
+		}
+	 } while (choice1 != 6);
+
+char ch ;
+while ((ch = getchar()) != '\n' && ch != EOF) ;
+printf("\n\nPress ENTER to continue.");
+while ((ch = getchar()) != '\n' && ch != EOF) ;
+
+}
+
+
+/******************************************* (4) DELETE *******************************************/
+
+void DELETE()
+{
+system("cls");
+    bubble_sort();
+    char target[128] ;
+    puts("\nEnter last name of the contact you want to delete : ");
+   scanf("%s",target);
+   int j=1;
+    for ( int i =0; i < counter; i++)
+    {
+        if ((strcmp(target,arr[i].last_name)==0))
+        {
+            printf("\nContacts with this last name [%s] : \n ",target);
+  printf("contact number.%d\n",i);
+  printf("first name is : %s \n",arr[i].first_name);
+  printf("last name is :%s \n",arr[i].last_name);
+  printf("Date of birth is %d-%d-%d \n",arr[i].BD.day,arr[i].BD.month,arr[i].BD.year);
+  printf("address is : %s\n",arr[i].adress);
+  printf("email is :%s \n",arr[i].email);
+  printf("mobile number is : %d\n",arr[i].mob_num);
+  printf("\n"); j++;
+        }
+    }
+    int k;
+    printf("what is contact number does you want to delete :");
+    scanf("%d",&k);
+for(k;k<counter-1;k++){
+    arr[k]=arr[k+1];
+}
+counter=counter-1;
+printf("\n\nDeleted successfully !\n");
+
+char ch ;
+while ((ch = getchar()) != '\n' && ch != EOF) ;
+printf("\n\nPress ENTER to continue.");
+while ((ch = getchar()) != '\n' && ch != EOF) ;
+}
+
+/*****************************************(5) PRINT FUNCTION *******************************************/
 void PRINT()
 {
-
+system("cls");
 // sorted fun
 bubble_sort();
 printf("\n\nPrinting entire Directory In sorted Order \n\n");
 for (int i=0 ;i<counter;i++)
 {
+
+  printf("No.%d\n",i+1);
   printf("first name is : %s \n",arr[i].first_name);
-  printf("last name is :%s  \n",arr[i].last_name);
+  printf("last name is :%s \n",arr[i].last_name);
+  printf("Date of birth is :%d-%d-%d \n",arr[i].BD.day,arr[i].BD.month,arr[i].BD.year);
   printf("address is : %s\n",arr[i].adress);
-  printf("city is :%s  \n",arr[i].city);
-  //printf("birth day is : \n",arr[i].first_name);
+  printf("email is :%s \n",arr[i].email);
   printf("mobile number is : %d \n",arr[i].mob_num);
+  printf("\n");
 
 
 }
@@ -169,18 +231,28 @@ printf("\n\nPress ENTER to continue.");
 while ((ch = getchar()) != '\n' && ch != EOF) ;
 
 }
-/******************************************* SEARCH FUNCTION *******************************************/
+/******************************************* (6)SEARCH FUNCTION *******************************************/
 void SEARCH()
 {
+    system("cls");
     bubble_sort();
     char target[128] ;
     puts("\nEnter last name you want to search with : ");
    scanf("%s",target);
+   int j=1;
     for ( int i =0; i < counter; i++)
     {
         if ((strcmp(target,arr[i].last_name)==0))
         {
-printf("%s  %s  %s  %s  %d \n ",arr[i].first_name,arr[i].last_name,arr[i].adress,arr[i].city,arr[i].mob_num);
+            printf("\nContacts with this last name [%s] : \n ",target);
+    printf("No.%d\n",j);
+  printf("first name is : %s \n",arr[i].first_name);
+  printf("last name is :%s \n",arr[i].last_name);
+  printf("Date of birth is %d-%d-%d \n",arr[i].BD.day,arr[i].BD.month,arr[i].BD.year);
+  printf("address is : %s\n",arr[i].adress);
+  printf("email is :%s \n",arr[i].email);
+  printf("mobile number is : %d\n",arr[i].mob_num);
+  printf("\n"); j++;
         }
     }
 
@@ -191,65 +263,48 @@ while ((ch = getchar()) != '\n' && ch != EOF) ;
 
 }
 
-/******************************************* MODIFY FUNCTION *******************************************/
 
 
-void MODIFY ()
+/******************************************* (7)SAVE FUNCTION *******************************************/
+
+void SAVE()
 {
-
-/** searching for this contact and return his index in structure **/
- int* index =(int*)malloc(100);
-    char target[128] ;
-    puts("\nEnter last name you want to search with : ");
-   scanf("%s",target);
-   int j=0 ;
-    for ( int i =0; i < counter; i++)
+    system("cls");
+FILE*fptr;
+fptr=fopen("directory.txt","a");
+printf("%d",counter);
+for(int i=0;i<counter;i++)
     {
-        if ((strcmp(target,arr[i].last_name)==0))
-        {
-            index[j]=i; j++;
-printf("no.%d to have this last name : %s %s %s %s %d \n ",j,arr[i].first_name,arr[i].last_name,arr[i].adress,arr[i].city,arr[i].mob_num);
-        }
+fprintf(fptr,"%s,%s,%d-%d-%d,%s,%s,%d\n",arr[i].first_name,arr[i].last_name,arr[i].BD.day,arr[i].BD.month,arr[i].BD.year,
+        arr[i].adress,arr[i].email,arr[i].mob_num);
+//fprintf(fptr,"\n");
     }
-    int n ;
-printf("what of them you want to modify in : ");
-scanf("%d",&n);
-int temp,m ;
-for (m=0;m<5;m++)
-{
-    if(n==m) temp=index[m];
-}
-cont* p=arr+(index[m]-1);
-int choice1;
-do
-{	printf("\n*** Enter what to modify ***\n\n");
-    printf( "1:Last name\n2:First name\n3:Address\n4:City\n5:Mobile number\n6:Nothing to modify\n" );
-    scanf("%d",&choice1);
-
-		switch(choice1)
-		{
-			case 1 : printf("enter new last name :");
-                     gets(p->last_name);
-		    case 2 : printf("enter new first name :");
-                     gets(p->first_name); ;
-            case 3 : printf("enter new address :");
-                     gets(arr[index[m]].city);
-            case 4 : printf("enter new city :");
-                     gets(arr[index[m]].city);
-            case 5 : printf("enter new mobile number :");
-                     scanf("%d",&p->mob_num);
-            case 6 :
-                printf("\nyou are quiting !\n");
-				return(0);
-			default: printf("\nInvalid choice!\n");
-				break;
-		}
-	 } while (choice1 != 7);
-
+    fclose(fptr);
+        system("cls");
+    printf("\n\nSaved successfully");
 char ch ;
 while ((ch = getchar()) != '\n' && ch != EOF) ;
 printf("\n\nPress ENTER to continue.");
 while ((ch = getchar()) != '\n' && ch != EOF) ;
-
 }
+/*******************************************************************************************************************************/
 
+
+/******************************************* SORT FUNCTION *******************************************/
+
+void bubble_sort()
+{
+for (int i=0 ; i<counter-1 ; i++)
+{
+    for (int j=0 ; j<counter-i-1;j++)
+    {
+        if((strcasecmp(arr[j].last_name,arr[j+1].last_name)>0))
+        {
+            cont temp ;
+            temp = arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}
+}
